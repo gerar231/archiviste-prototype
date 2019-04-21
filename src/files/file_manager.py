9ef74de0041dd101767766a5a31dda850e31f76b
Gerard <gerar231@gmail.com>
2019-04-21 14:49:44 -0700

    Added v1 implementation of get_project_files.

--- src/files/file_manager.py
+++ src/files/file_manager.py
@@ -1,13 +1,16 @@
+import os, sys
 from typing import List
 
 class FileManager(object):
     """
-        Extracts meaningful data from raw media files for use by DataAnalyzer given file directories.
+        Extracts meaningful data from raw media files for use by DataAnalyzer 
+        given file directories.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
             Initializes this FileManager instance.
         """
+        self.known_video_paths = dict()
         print("FileManager.__init__()")
     
     def get_project_files(self, path: str) -> List[str]:
@@ -15,13 +18,22 @@ class FileManager(object):
             Keyword Arguments:
                 path: the file path of a directory containing video files to load.
 
-            Returns a list of valid video file paths in the provided directory path, 
-            recursive directory loading not yet supported. ValueError if path is 
-            invalid or not a directory.
+            Returns a list of valid video file paths in the provided directory 
+            path which are added to the known video paths. Assumes all files in 
+            folder are video files, recursive directory loading not yet supported. 
+            ValueError if path is invalid or not a directory.
         """
         print("FileManager.get_project_files()")
-        # verify valid
         # verify directory
+        if (not os.path.isdir(path)):
+            return ValueError("""Invalid path passed to 
+                FileManager.get_project_files().""")
         # fetch all video file paths
-        # return a list of video file paths
-        return list()
+        video_paths = {p for f in os.listdir(path) 
+            for p in os.path.join(path, f) if os.path.isfile(p)}
+        if self.known_video_paths.get(path) is None:
+            self.known_video_paths[path] = video_paths
+        else:
+            self.known_video_paths[path] = self.known_video_paths[path].union(video_paths)
+        print(self.known_video_paths)
+        return video_paths
